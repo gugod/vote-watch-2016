cb6e9329a2d16a1357dee4088f720961cc186052
Kang-min Liu <gugod@gugod.org>
2016-01-16 09:54:58 +0100

    output fix.

--- run.pl
+++ run.pl
@@ -10,6 +10,7 @@ my $json = JSON::PP->new->canonical->pretty;
 my $watchlist = $json->decode( scalar read_file("${FindBin::Bin}/watchlist.json") );
 
 my $http = HTTP::Tiny->new;
+
 for my $k (keys %$watchlist) {
     my $urls = $watchlist->{$k};
 
@@ -17,14 +18,16 @@ for my $k (keys %$watchlist) {
         $urls = [$urls];
     }
     for my $url (@$urls) {
-        my ($fn) = $url =~ m{/(.+)\.html$};
-        say "$k => $url";
+        my ($fn) = $url =~ m{/([^/]+)\.html$};
         my $now = time;
         my ($sec, $min, $hour, $mday, $mon, $year) = gmtime(time);
         $year += 1900;
         $mon += 1;
         my $output_dir = sprintf('data/%s/%s/%04d%02d%02d%02d%02d%02d', $k, $fn, $year, $mon, $mday, $hour, $min, $sec);
+
         make_path($output_dir) unless -d $output_dir;
+
+        say "$k => $url => $output_dir";
         my $res = $http->get($url);
         my $res_dump = $json->encode($res);
         write_file "${output_dir}/http-response.json", $res_dump;
