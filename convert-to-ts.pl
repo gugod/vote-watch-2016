e49005690d7129e76f9e5de018476666f9d4855e
Kang-min Liu <gugod@gugod.org>
2016-01-16 20:19:16 +0100

    program to convert collected data to graphite format.

---
+++ convert-to-ts.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/env perl
+use v5.18;use strict;use warnings;use autodie;
+use File::Slurp qw<read_file>;
+use Mojo::DOM;
+use File::Next;
+use Time::Moment;
+
+sub epoch {
+    my ($t) = @_;
+    my $tm = Time::Moment->new(
+        year   => substr($t, 0, 4),
+        month  => substr($t, 4, 2),
+        day    => substr($t, 6, 2),
+        hour   => substr($t, 8, 2),
+        minute => substr($t, 10, 2),
+        second => substr($t, 12, 2),
+        offset => 0,
+    );
+    return $tm->epoch;
+}
+
+sub MAIN {
+    my $files = File::Next::files('data');
+    while (defined( my $file = $files->() )) {
+        my ($what, $branch, $t) = $file =~ m{data/([^/]+)/([^/]+)/([0-9]{14}+)/page\.html};
+        next unless $what && $branch && $t;
+
+        # say "$what - $branch - $t --- $file";
+        my $html = read_file($file, { binmode => ":utf8" });
+        my $dom  = Mojo::DOM->new($html);
+
+        $dom->find("tr.trT")->each(
+            sub {
+                my @cells = $_->find("td")->map('text')->each;
+                my ($name, $value) = grep { /\d+/ } @cells;
+                $value =~ s/,//;
+                say "${what}.${branch}.${name} ${value} " . epoch($t);
+            }
+        );    
+    }
+}
+MAIN();
